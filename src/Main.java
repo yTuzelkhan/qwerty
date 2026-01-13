43f037b202a51b13107722b3f67f2cf5a44d0f29
Yermek Tuzelkhan <252080@astanait.edu.kz>
2026-01-13 08:23:12 +0500

    ////

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
   //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
+  // to see how IntellifdsdfsfdsJ IDEA suggests fixing it.
   IO.println(String.format("Hello and welcome!"));
 
   for (int i = 1; i <= 5; i++) {
